3b10caa1532dcc6f77587a0fe387da5cfd152d6a
sai tharun <saitharung96@gmail.com>
2025-06-15 10:57:33 +0000

    Error: Build process exited with error code 1.     at ChildProcess.<anon

--- src/components/PwaRegistry.tsx
+++
@@ -1,4 +0,0 @@
-// This component was used for manual service worker registration.
-// It is no longer needed as `@ducanh2912/next-pwa` now handles PWA setup.
-// You can safely delete this file (src/components/PwaRegistry.tsx).
-export {};
