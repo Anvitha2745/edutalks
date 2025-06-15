fa31845589ab3e288e9c58f949a53fc8e6eb0810
sai tharun <saitharung96@gmail.com>
2025-06-15 09:54:30 +0000

    https://console.cloud.google.com/cloud-build/builds;region=us-central1/e

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+
+import type {NextConfig} from 'next';
+
+const nextAppConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false, // Set to false to surface TypeScript errors
+  },
+  eslint: {
+    ignoreDuringBuilds: false, // Set to false to surface ESLint errors
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  allowedDevOrigins: [
+    'http://9000-firebase-studio-1749402915609.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev',
+    'http://6000-firebase-studio-1749402915609.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev'
+  ],
+};
+
+// PWA related code has been removed for build diagnosis.
+// If PWA functionality is needed later, it will need to be re-introduced carefully.
+const finalConfig = nextAppConfig;
+
+export default finalConfig;
